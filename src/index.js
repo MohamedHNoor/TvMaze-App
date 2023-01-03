393a5c6823a3c5d403bb640a10c37d4a9bda52c2
Yash <74345713+yash244466666@users.noreply.github.com>
2023-01-03 16:31:18 +0600

    Add header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+// import '../assets/res/logo.png';
 import './style.css';
+// import header from './modules/header';
+
+// let body = document.getElementsByTagName('body');
+
+// let headerSection = document.createElement('header-section');
+
+// headerSection.innerHTML = header;
+
+  
--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+
     ],
   },
 };

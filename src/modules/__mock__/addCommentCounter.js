8df281f579d9a4386774fea34258c9ced3aeffbb
Yash <74345713+yash244466666@users.noreply.github.com>
2023-01-05 15:06:42 +0600

    Fix linter errors

--- src/modules/__mock__/addCommentCounter.js
+++ src/modules/__mock__/addCommentCounter.js
@@ -1,7 +1,7 @@
 const addCommentCounter = () => {
-    const commentItems = document.querySelectorAll('.comment_paragraph');
-    const numberOfComments = commentItems.length;
-    return numberOfComments;
-  };
-  
-  export default addCommentCounter;
+  const commentItems = document.querySelectorAll('.comment_paragraph');
+  const numberOfComments = commentItems.length;
+  return numberOfComments;
+};
+
+export default addCommentCounter;

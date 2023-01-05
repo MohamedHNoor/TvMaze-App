1de8545acafcb01c815db3af16be6f17193fc5e1
Yash <74345713+yash244466666@users.noreply.github.com>
2023-01-05 14:43:42 +0600

    Add mock folder with comment counter file

---
+++ src/modules/__mock__/addCommentCounter.js
@@ -0,0 +1,7 @@
+const addCommentCounter = () => {
+    const commentItems = document.querySelectorAll('.comment_paragraph');
+    const numberOfComments = commentItems.length;
+    return numberOfComments;
+  };
+  
+  export default addCommentCounter;
